c945cc6604648bea0fbd5d0586a7d7017dc9bf9e
mao <1296193245@qq.com>
2022-10-10 19:07:03 +0800

    android_SmsManager

--- app/src/main/java/mao/android_smsmanager/MainActivity.java
+++ app/src/main/java/mao/android_smsmanager/MainActivity.java
@@ -1,13 +1,19 @@
 package mao.android_smsmanager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.view.View;
@@ -24,6 +30,8 @@ public class MainActivity extends AppCompatActivity
     private BroadcastReceiver broadcastReceiver;
     private PendingIntent deliverPI;
     private BroadcastReceiver broadcastReceiver1;
+    private EditText editText1;
+    private EditText editText2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -31,27 +39,36 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText editText1 = findViewById(R.id.EditText1);
-        EditText editText2 = findViewById(R.id.EditText2);
+        editText1 = findViewById(R.id.EditText1);
+        editText2 = findViewById(R.id.EditText2);
 
         findViewById(R.id.Button).setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-                String phone = editText1.getText().toString();
-                String content = editText2.getText().toString();
-                if (phone.length() == 0)
-                {
-                    return;
-                }
-                if (content.length() == 0)
-                {
-                    return;
-                }
-                sendSMS(phone, content);
+                sendSMS(R.id.Button);
             }
         });
+    }
+
+
+    /**
+     * 发送短信
+     */
+    public void sendSMS()
+    {
+        String phone = editText1.getText().toString();
+        String content = editText2.getText().toString();
+        if (phone.length() == 0)
+        {
+            return;
+        }
+        if (content.length() == 0)
+        {
+            return;
+        }
+        sendSMS(phone, content);
     }
 
 
@@ -134,5 +151,131 @@ public class MainActivity extends AppCompatActivity
         super.onStop();
         unregisterReceiver(broadcastReceiver);
         unregisterReceiver(broadcastReceiver1);
+    }
+
+
+    /**
+     * 发送短信
+     *
+     * @param requestCode 请求代码
+     */
+    public void sendSMS(int requestCode)
+    {
+        if (checkPermission(MainActivity.this, Manifest.permission.SEND_SMS,
+                requestCode % 65536))
+        {
+            sendSMS();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        // requestCode不能为负数，也不能大于2的16次方即65536
+        if (requestCode == R.id.Button % 65536)
+        {
+            if (checkGrant(grantResults))
+            {
+                sendSMS();
+            }
+            else
+            {
+                toastShow("没有权限");
+            }
+        }
+    }
+
+
+    /**
+     * 检查权限结果数组，
+     *
+     * @param grantResults 授予相应权限的结果是PackageManager.PERMISSION_GRANTED
+     *                     或PackageManager.PERMISSION_DENIED
+     *                     从不为空
+     * @return boolean 返回true表示都已经获得授权 返回false表示至少有一个未获得授权
+     */
+    public static boolean checkGrant(int[] grantResults)
+    {
+        boolean result = true;
+        if (grantResults != null)
+        {
+            for (int grant : grantResults)
+            {
+                //遍历权限结果数组中的每条选择结果
+                if (grant != PackageManager.PERMISSION_GRANTED)
+                {
+                    //未获得授权，返回false
+                    result = false;
+                    break;
+                }
+            }
+        }
+        else
+        {
+            result = false;
+        }
+        return result;
+    }
+
+
+    /**
+     * 检查某个权限
+     *
+     * @param act         Activity对象
+     * @param permission  许可
+     * @param requestCode 请求代码
+     * @return boolean 返回true表示已启用该权限，返回false表示未启用该权限
+     */
+    public static boolean checkPermission(Activity act, String permission, int requestCode)
+    {
+        return checkPermission(act, new String[]{permission}, requestCode);
+    }
+
+
+    /**
+     * 检查多个权限
+     *
+     * @param act         Activity对象
+     * @param permissions 权限
+     * @param requestCode 请求代码
+     * @return boolean 返回true表示已完全启用权限，返回false表示未完全启用权限
+     */
+    @SuppressWarnings("all")
+    public static boolean checkPermission(Activity act, String[] permissions, int requestCode)
+    {
+        boolean result = true;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            int check = PackageManager.PERMISSION_GRANTED;
+            //通过权限数组检查是否都开启了这些权限
+            for (String permission : permissions)
+            {
+                check = ContextCompat.checkSelfPermission(act, permission);
+                if (check != PackageManager.PERMISSION_GRANTED)
+                {
+                    //有个权限没有开启，就跳出循环
+                    break;
+                }
+            }
+            if (check != PackageManager.PERMISSION_GRANTED)
+            {
+                //未开启该权限，则请求系统弹窗，好让用户选择是否立即开启权限
+                ActivityCompat.requestPermissions(act, permissions, requestCode);
+                result = false;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 显示消息
+     *
+     * @param message 消息
+     */
+    private void toastShow(String message)
+    {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
